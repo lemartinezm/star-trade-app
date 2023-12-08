6c606006d415cb52d845e4adbc2b3918a6934bef
Luis Martínez <lemartinezmendoza@gmail.com>
2023-12-08 14:49:39 -0500

    feat: add overview and transactions pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { OverviewComponent } from './dashboard/pages/overview/overview.component';
+import { TransactionsComponent } from './dashboard/pages/transactions/transactions.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: DashboardComponent },
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      { path: 'overview', component: OverviewComponent },
+      { path: 'transactions', component: TransactionsComponent },
+    ],
+  },
 ];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
