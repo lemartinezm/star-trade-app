1640e0443e74f29759355618d6024569a3501fd1
Luis Martínez <lemartinezmendoza@gmail.com>
2023-12-12 10:59:00 -0500

    chore: add form control to form input component

--- src/app/shared/components/form-input/form-input.component.ts
+++ src/app/shared/components/form-input/form-input.component.ts
@@ -1,17 +1,23 @@
-import { Component, Input } from '@angular/core';
-import { InputComponent } from '../input/input.component';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-input',
   standalone: true,
-  imports: [InputComponent],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-input.component.html',
   styleUrl: './form-input.component.css',
 })
-export class FormInputComponent {
+export class FormInputComponent implements OnInit {
   @Input() label: string = 'Label';
   @Input() inputId: string = '';
   @Input() name: string = '';
   @Input() type: string = 'text';
   @Input() placeholder: string = '';
+  @Input() parentFormGroup!: FormGroup;
+  inputControl = new FormControl('');
+
+  ngOnInit() {
+    this.parentFormGroup.addControl(this.name, this.inputControl);
+  }
 }
