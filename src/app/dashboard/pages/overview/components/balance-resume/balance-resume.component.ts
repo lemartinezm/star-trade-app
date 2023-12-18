085980d0f570b4ed371cf03f6597bdd7176941e3
Luis Martínez <lemartinezmendoza@gmail.com>
2023-12-18 09:34:49 -0500

    fix: update balance resume component

--- src/app/dashboard/pages/overview/components/balance-resume/balance-resume.component.ts
+++ src/app/dashboard/pages/overview/components/balance-resume/balance-resume.component.ts
@@ -28,7 +28,7 @@ export class BalanceResumeComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.overviewService.userAccounts.subscribe({
+    this.balanceResumeService.getAccounts().subscribe({
       next: (accounts) => {
         this.accounts = [...accounts];
         this.accountSelected = accounts[0];
@@ -37,11 +37,6 @@ export class BalanceResumeComponent implements OnInit {
           value: account.accountNumber,
         }));
         this.overviewService.updateState(accounts[0].accountNumber);
-      },
-    });
-
-    this.balanceResumeService.getAccounts().subscribe({
-      next: (accounts) => {
         this.overviewService.updateUserAccounts(accounts);
       },
       error: (err) => console.error(err),
