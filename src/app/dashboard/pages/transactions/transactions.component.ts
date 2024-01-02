c7cd4d400ad8a40d87d064b7242a00ae8167d4ef
Luis Martínez <lemartinezmendoza@gmail.com>
2024-01-02 11:07:01 -0500

    feat: add filters mockup to transactions table

--- src/app/dashboard/pages/transactions/transactions.component.ts
+++ src/app/dashboard/pages/transactions/transactions.component.ts
@@ -4,11 +4,20 @@ import { TransactionsService } from './transactions.service';
 import { NgClass, NgFor } from '@angular/common';
 import { Account } from '../overview/interfaces/account.interface';
 import { MetaData } from '../../../shared/interfaces/response.interface';
+import { FormInputComponent } from '../../../shared/components/form-input/form-input.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormSelectComponent } from '../../../shared/components/form/form-select/form-select.component';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [NgFor, NgClass],
+  imports: [
+    NgFor,
+    NgClass,
+    FormInputComponent,
+    FormSelectComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css',
   providers: [TransactionsService],
@@ -27,6 +36,18 @@ export class TransactionsComponent implements OnInit {
     nextPage: 1,
     previousPage: 1,
   };
+
+  filtersForm = new FormGroup({
+    startDate: new FormControl(''),
+    endDate: new FormControl(''),
+    transactionsType: new FormControl('all'),
+  });
+
+  transactionsTypeOptions = [
+    { label: 'All', value: 'all' },
+    { label: 'Expenses', value: 'expenses' },
+    { label: 'Incomes', value: 'incomes' },
+  ];
 
   constructor(private transactionsService: TransactionsService) {}
 
